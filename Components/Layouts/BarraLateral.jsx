793e3ded64796bd66be67566c1ba38150bb7744a
Caleb Martínez Cavazos <calebmartinez0929@gmail.com>
2021-02-22 16:34:17 -0600

    Sidebar redondeada, hover, y texto mas grande menos del título

--- Components/Layouts/BarraLateral.jsx
+++ Components/Layouts/BarraLateral.jsx
@@ -5,34 +5,60 @@ import styled from '@emotion/styled';
 const Contenedor = styled.div`
     background-color: rgba(43,53,89,255);
     height: 50rem;
+    border-radius: 4rem;
 `;
 const Espacio = styled.div`
     display: flex;
     align-items: center;
     height: 100vh;
 `; 
+const PreNav = styled.div`
+    height: 30rem;
+`;
+const Navegacion = styled.nav`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+    font-size: 1.6rem;
+`;
+const StyledLink = styled.a`
+   transition: .2s ease-in;
+   padding: 2.05rem 0;
+   border-radius: 2rem;
+
+   &:hover {
+       background-color: white;
+       color: black;
+   }
+`;
+//ARREGLA EL WIDTH DEL ESPACIO!!!//
 
 const BarraLateral = () => {
     return ( 
         <>
+            {/*El espacio es el area de 100vh donde se encuentra la Barra lateral*/}
             <Espacio>
-                <Contenedor className="ml-20 w-80 text-white flex flex-col text-center">
-                    <div className="h-60">
+                <Contenedor className="ml-20 w-96 text-white flex flex-col text-center">
+
+                    {/*La etiqueta PreNav es para el espacio del título y del logo*/}
+                    <PreNav>
                         <h1>Septum</h1>
 
                         <img src="/" alt="Logo"/>
-                    </div>
+                    </PreNav>
 
-                    <nav className="flex flex-col text-center">
-                        <Link href="/">Regístrate</Link>
-                        <Link href="/materias">Materias</Link>
-                        <Link href="/banco-de-preguntas">Banco de Preguntas</Link>
-                        <Link href="/contacto">Contacto</Link>
-                    </nav>
+                    {/*Para ponerle estilos a los Link hay que agregar despues del href la etiqueta passHref y al crear el styled component se lo asignas a un "a" y luego la etiqueta del styled component la pones alrededor del html, o sea dentro del link*/}
+                    <Navegacion className="text-center py-12">
+                        <Link href="/" passHref><StyledLink>Regístrate</StyledLink></Link>
+                        <Link href="/materias" passHref><StyledLink>Materias</StyledLink></Link>
+                        <Link href="/banco-de-preguntas" passHref><StyledLink>BP</StyledLink></Link>
+                        <Link href="/contacto" passHref><StyledLink>Contacto</StyledLink></Link>
+                    </Navegacion>
                 </Contenedor>
             </Espacio>
         </>
      );
 }
- 
+
 export default BarraLateral;
