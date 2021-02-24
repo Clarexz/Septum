cb03602ae3477b8e0ac4ffc0ed398329a5a6c6db
Caleb Martínez Cavazos <calebmartinez0929@gmail.com>
2021-02-23 23:22:42 -0600

    Barra lateral LISTA

--- Components/Layouts/Layout.jsx
+++ Components/Layouts/Layout.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import Link from 'next/link';
 import BarraLateral from './BarraLateral';
+import Head from 'next/head';
 
 const Layout = props => {
     return ( 
         <>
+            <Head>
+                <html lang="es"/>
+                <title>Septum</title>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet"/>
+            </Head>
+
             <BarraLateral>
             </BarraLateral>
 
